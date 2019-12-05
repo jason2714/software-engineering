7856a6a406ff7356d68b4d47341f9e7898f1a7c4
jason2714 <jason27146913@gmail.com>
2019-12-05 16:01:12 +0800

    菜單

--- 菜單管理頁面.js
+++ 菜單管理頁面.js
@@ -1,5 +1,5 @@
 var item_type, item_name, item_price, item_picture, item_info;
-var item_upload;
+var item_upload, item_upload_src;
 var table_width = [10, 20, 30, 15, 15, 10];
 var item_ex = [
     ["三明治", "牛肉三明治", "35", "pic/1.jpg", "好ㄘ"],
@@ -41,23 +41,33 @@ var item_length = 6;
 var tbody;
 
 function item_add() {
+    if (!item_price.value || !item_name.value || !item_upload_src || !item_info.value) {
+        window.alert("資料填寫不完全");
+        return;
+    }
+
     let ito = item_type.getElementsByTagName("option");
     let it;
     for (let i in item_type_name[0])
         if (item_type_name[1][i] == item_type.value)
             it = item_type_name[0][i];
-    let arr = [it, item_name.value, item_price.value, item_picture.value, item_info.value];
+    let arr = [it, item_name.value, item_price.value, item_upload_src, item_info.value];
+    console.log(arr);
     item_ex.push(arr);
+    console.log(item_ex);
     ito[0].selected = true;
     item_name.value = "";
     item_price.value = "";
     item_picture.value = "";
+    item_upload_src = "";
     item_info.value = "";
     buildtbody(item_ex);
 }
 
-function uploadFile() {
-    let pic_src = item_upload.value;
+function uploadFile(ev) {
+    console.log(ev.target.files, ev.target.files[0]);
+    item_upload_src = item_upload.value;
+    let pic_src = item_upload_src;
     pic_src = pic_src.split("\\");
     item_picture.value = pic_src[pic_src.length - 1];
 }
@@ -124,16 +134,50 @@ function buildtbody(tdata) {
 }
 
 function addToCombo(ev) {
+    let parent = ev.target.parentNode.parentNode;
+
 
 }
 
 function itemEdit(ev) {
+    if (item_price.value || item_name.value || item_upload_src || item_info.value) {
+        if (!window.confirm("資料欄位中有值，確定覆蓋?"))
+            return;
+    }
     let parent = ev.target.parentNode.parentNode;
-    console.log(parent);
+    for (let i in item_ex) {
+        if (item_ex[i][1] == parent.id) {
+            for (let j in item_type_name[0])
+                if (item_type_name[0][j] == item_ex[i][0])
+                    item_type.value = item_type_name[1][j];
+            item_name.value = item_ex[i][1];
+            item_price.value = item_ex[i][2];
+            item_upload_src = item_ex[i][3];
+            let pic_src = item_upload_src;
+            console.log(item_upload_src);
+            pic_src = pic_src.split("\\");
+            item_picture.value = pic_src[pic_src.length - 1];
+            item_info.value = item_ex[i][4];
+            item_ex.splice(i, 1);
+            break;
+        }
+    }
+    parent.parentNode.removeChild(parent);
 }
 
 function itemRemove(ev) {
-
+    if (!window.confirm("確定要刪除此餐點嗎?"))
+        return;
+    let parent = ev.target.parentNode.parentNode;
+    parent.parentNode.removeChild(parent);
+    /*for (let i in item_ex) {
+        if (item_ex[i][1] == parent.id) {
+            item_ex.splice(i, 1);
+            console.log(item_ex);
+            break;
+        }
+    }
+    buildtbody(item_ex);*/
 }
 
 function buildthead(select) {
@@ -144,4 +188,5 @@ function buildthead(select) {
         select.appendChild(option);
     }
 }
+
 window.addEventListener("load", start, false);
