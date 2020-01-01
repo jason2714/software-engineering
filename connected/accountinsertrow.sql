bc81f941f18f5e2da2fa8f87a0490b146ff78e12
jason2714 <jason27146913@gmail.com>
2020-01-01 15:44:33 +0800

    modify

--- connected/accountinsertrow.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO account(user_ID,password,name,age,gender,email,total) VALUES("0975975176","123","傑森史塔森",20,"男","sss@gmail.com",1000);
-INSERT INTO account(user_ID,password,name,age,gender,email,total) VALUES("0987987987","456","小瑞",25,"女","156785978@gmail.com",40000);
-INSERT INTO account(user_ID,password,name,age,gender,email,total) VALUES("0987654321","777","中瑞",24,"男","aaaaa@gmail.com",40000);
-INSERT INTO account(user_ID,password,name,age,gender,email,total) VALUES("09123456789","7890","大瑞",18,"女","aaaaa@gmail.com",7890);
--- connected/login.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php
-  $hn = 'localhost';
-  $db = 'XBS-system';
-  $un = 'jason2714'; // Change this
-  $pw = 'winnie434498'; // Change this
-?>
